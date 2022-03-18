91738b5ab41869fde30a78410934560a1b515dc0
Stanislav Rakitov <prosperousrf@gmail.com>
2022-03-18 22:25:01 +0300

    all modules

---
+++ db/src/main/java/DbSetting.java
@@ -0,0 +1,14 @@
+/**
+ * @author Stanislav Rakitov in 2022
+ */
+public class DbSetting {
+
+  private String name;
+  private String password;
+
+  public DbSetting(String name, String password) {
+    this.name = name;
+    this.password = password;
+  }
+
+}
